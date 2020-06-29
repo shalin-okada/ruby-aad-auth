ae370d2e9d14ddc92fd04f5423a6fd5f8e9a0a8d
Shalin Okada <shalinokada@gmail.com>
2020-06-30 04:12:56 +0900

    :fire: remote a test

--- spec/aad_auth_spec.rb
+++ spec/aad_auth_spec.rb
@@ -1,24 +1,2 @@
 RSpec.describe AadAuth do
-  describe 'validation' do
-    context 'ENV["APP_ID"] is null' do
-      
-    end
-
-    context 'ENV["TENANT_ID"] is null' do
-      
-    end
-
-    context 'Old token.' do
-      
-    end
-
-    context 'Different application token.' do
-      
-    end
-
-    context "Different tenant token." do
-      
-    end
-    
-  end
 end
